42c1409b55210e98a89cf7ac5c895e5a2e791a03
Tom Kisha <x0rsw1tch@gmail.com>
2021-02-16 18:56:07 -0500

    functions to get category/tags for each post

--- wp-to-json.php
+++ wp-to-json.php
@@ -6,14 +6,63 @@ try {
 	echo "something went wrong";
 }
 
+require './get-metadata.php';
+
 $posts = array();
 
-$allPostsSql = $db->prepare('SELECT * FROM wp_posts WHERE `post_status` = "publish" AND post_content != "{{unknown}}" AND post_content != ""');
-$allPostsSql->execute();
-$posts = $allPostsSql->fetchAll();
+function getPostMetaIdentifiers (PDO $db, Int $postId) {
+	$postQuery = $db->prepare('SELECT * FROM wp_term_relationships WHERE object_id = :postId');
+	$postQuery->bindParam(":postId", $postId);
+	$postQuery->execute();
+	return $postQuery->fetchAll(PDO::FETCH_ASSOC);
+}
 
-foreach ($posts as $post) {
-	echo $post['post_title'];
+function getPostCategory (PDO $db, Int $postId, array $categories) {
+	$postTaxonomy = getPostMetaIdentifiers($db, $postId);
+	foreach ($postTaxonomy as $taxonomyEntry) {
+		foreach ($categories as $category) {
+			if ($taxonomyEntry['term_taxonomy_id'] === $category['term_id']) {
+				return $category;
+			}
+		}
+	}
+}
+
+function getPostTags (PDO $db, Int $postId, array $tags) {
+	$out = array();
+	$postTaxonomy = getPostMetaIdentifiers($db, $postId);
+	foreach ($postTaxonomy as $taxonomyEntry) {
+		foreach ($tags as $tag) {
+			if ($taxonomyEntry['term_taxonomy_id'] === $tag['term_id']) {
+				array_push($out, $tag);
+			}
+		}
+	}
+	return $out;
+}
+
+function getPostAuthor (PDO $db, Int $authorId) {
+	$out = array();
+	return $out;
+}
+
+foreach ($allPosts as $post) {
+	$out = Array(
+		'stName' => 'Blog',
+		'title' => $post['post_title'],
+		'urlTitle' => $post['post_name'],
+		'body' => $post['post_content'],
+		'postDate' => $post['post_date'],
+		'publishDate' => $post['post_date'],
+		'legacyId' => $post['ID'],
+		'legacyGuid' => $post['guid'],
+		'legacyAuthor' => $post['post_author'],
+		'category' => getPostCategory($db, $post['ID'], $categories),
+		'tags' => getPostTags($db, $post['ID'], $tags),
+		'author' => getPostAuthor($db, $post['post_author'])
+	);
+
+	array_push($posts, $out);
 }
 
 
