19c345f83a39b9e8bf004fba8b8643034bfd68d5
Tom Kisha <x0rsw1tch@gmail.com>
2021-02-16 21:39:38 -0500

    functions file

---
+++ functions.php
@@ -0,0 +1,64 @@
+<?php
+function getYoastMetadata (PDO $db, Int $postId) {
+	$yoastQuery = $db->prepare("SELECT * FROM wp_yoast_indexable WHERE `object_id` = :postId");
+	$yoastQuery->bindParam(":postId", $postId);
+	$yoastQuery->execute();
+	return $yoastQuery->fetch(PDO::FETCH_ASSOC);
+}
+
+
+function getSEOTitle (PDO $db, Int $postId, string $originalTitle) {
+	$postYoast = getYoastMetadata($db, $postId);
+	if ($postYoast['title'] !== null) {
+		return $postYoast['title'];
+	}
+	return $originalTitle;
+}
+
+function getSEODescription (PDO $db, Int $postId) {
+	$postYoast = getYoastMetadata($db, $postId);
+	if ($postYoast['description'] !== null) {
+		return $postYoast['description'];
+	}
+	return "";
+}
+
+function getPostMetaIdentifiers (PDO $db, Int $postId) {
+	$postQuery = $db->prepare('SELECT * FROM wp_term_relationships WHERE object_id = :postId');
+	$postQuery->bindParam(":postId", $postId);
+	$postQuery->execute();
+	return $postQuery->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function getPostCategory (PDO $db, Int $postId, array $categories) {
+	$postTaxonomy = getPostMetaIdentifiers($db, $postId);
+	foreach ($postTaxonomy as $taxonomyEntry) {
+		foreach ($categories as $category) {
+			if ($taxonomyEntry['term_taxonomy_id'] === $category['term_id']) {
+				return $category;
+			}
+		}
+	}
+}
+
+function getPostTags (PDO $db, Int $postId, array $tags) {
+	$out = array();
+	$postTaxonomy = getPostMetaIdentifiers($db, $postId);
+	foreach ($postTaxonomy as $taxonomyEntry) {
+		foreach ($tags as $tag) {
+			if ($taxonomyEntry['term_taxonomy_id'] === $tag['term_id']) {
+				array_push($out, $tag);
+			}
+		}
+	}
+	return $out;
+}
+
+function getPostAuthor (Int $authorId, array $authors) {
+	foreach ($authors as $author) {
+		if ($author['ID'] === $authorId) {
+			return $author;
+		}
+	}
+}
+?>
